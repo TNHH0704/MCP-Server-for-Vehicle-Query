55ed0e497e78452dfb001484f686d3d5a231af76
TNHH0704 <hoang.trinhnguyen.cit22@eiu.edu.vn>
2026-01-16 12:03:39 +0700

    bug fixes to work with docker

--- Program.cs
+++ Program.cs
@@ -9,6 +9,28 @@ builder.Logging.AddConsole(consoleLogOptions =>
 {
     consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Trace;
 });
+
+builder.Services.AddHttpClient();
+builder.Services.AddTransient<McpVersionVer2.Services.VehicleMapperService>();
+
+builder.Services.AddTransient<McpVersionVer2.Services.VehicleStatusService>(sp => 
+    new McpVersionVer2.Services.VehicleStatusService(sp.GetRequiredService<System.Net.Http.IHttpClientFactory>().CreateClient(), sp.GetRequiredService<Microsoft.Extensions.Configuration.IConfiguration>()));
+
+builder.Services.AddTransient<McpVersionVer2.Services.VehicleStatusMapperService>();
+
+builder.Services.AddTransient<McpVersionVer2.Services.WaypointService>(sp => 
+    new McpVersionVer2.Services.WaypointService(sp.GetRequiredService<System.Net.Http.IHttpClientFactory>().CreateClient(), sp.GetRequiredService<Microsoft.Extensions.Logging.ILogger<McpVersionVer2.Services.WaypointService>>(), sp.GetRequiredService<Microsoft.Extensions.Configuration.IConfiguration>()));
+
+builder.Services.AddTransient<McpVersionVer2.Services.VehicleHistoryService>();
+
+builder.Services.AddTransient<McpVersionVer2.Services.AuthService>(sp => 
+    new McpVersionVer2.Services.AuthService(sp.GetRequiredService<System.Net.Http.IHttpClientFactory>().CreateClient(), sp.GetRequiredService<Microsoft.Extensions.Logging.ILogger<McpVersionVer2.Services.AuthService>>(), sp.GetRequiredService<Microsoft.Extensions.Configuration.IConfiguration>()));
+
+builder.Services.AddTransient<McpVersionVer2.Services.VehicleService>(sp => 
+    new McpVersionVer2.Services.VehicleService(sp.GetRequiredService<System.Net.Http.IHttpClientFactory>().CreateClient(), sp.GetRequiredService<Microsoft.Extensions.Logging.ILogger<McpVersionVer2.Services.VehicleService>>(), sp.GetRequiredService<Microsoft.Extensions.Configuration.IConfiguration>()));
+
+builder.Services.AddTransient<McpVersionVer2.Services.GisUtil>();
+
 builder.Services
     .AddMcpServer()
     .WithStdioServerTransport()
